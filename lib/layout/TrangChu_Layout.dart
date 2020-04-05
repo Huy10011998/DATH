4d9d34d324efd8d9f47b6b527fc2434bf2ca2f24
phutruongck <phutruongck@gmail.com>
2020-04-05 15:50:56 +0700

    feat: update load more

--- lib/layout/TrangChu_Layout.dart
+++ lib/layout/TrangChu_Layout.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:test1/layout/size_config.dart';
 
 class TrangchuLayout extends StatefulWidget {
   @override
@@ -8,10 +9,11 @@ class TrangchuLayout extends StatefulWidget {
 }
 
 class _TrangchuLayoutState extends State<TrangchuLayout> {
-  final List<String> myList = List<String>.generate(7, (index) => "");
+  final List<String> myList = List<String>.generate(6, (index) => "myData");
   List<String> items = List<String>();
   int perPage = 2;
   int present = 0;
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -22,7 +24,8 @@ class _TrangchuLayoutState extends State<TrangchuLayout> {
     });
   }
 
-  _getMoreData() {
+  Future _getMoreData() async {
+    await new Future.delayed(new Duration(seconds: 1));
     setState(() {
       if ((present + perPage) > myList.length) {
         items.addAll(myList.getRange(present, myList.length));
@@ -30,15 +33,21 @@ class _TrangchuLayoutState extends State<TrangchuLayout> {
         items.addAll(myList.getRange(present, present + perPage));
       }
       present = present + perPage;
+      isLoading = false;
     });
   }
 
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return new Scaffold(
       body: NotificationListener<ScrollNotification>(
         onNotification: (ScrollNotification scrollInfo) {
-          if (scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
+          if (!isLoading &&
+              scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
             _getMoreData();
+            setState(() {
+              isLoading = true;
+            });
           }
           return;
         },
@@ -52,134 +61,139 @@ class _TrangchuLayoutState extends State<TrangchuLayout> {
                 itemBuilder: (BuildContext context, int index) {
                   if (index == items.length)
                     return Container(
+                        height: isLoading ? 50.0 : 0,
+                        color: Colors.transparent,
+                        child: Center(
+                          child: new CircularProgressIndicator(),
+                        ));
+                  if (index > items.length)
+                    return Container(
                       color: Colors.greenAccent,
-                      child: Text("Load More"),
+                      child: Text("Da het data"),
                     );
-                  else
-                    return InkWell(
-                      child: Container(
-                        padding: EdgeInsets.only(top: 10, left: 5, right: 5),
-                        child: new Card(
-                          child: Container(
-                            child: new Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Container(
-                                    child: Row(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Icon(
-                                          Icons.place,
-                                          color: Colors.grey,
+                  return new InkWell(
+                    child: Container(
+                      padding: EdgeInsets.only(top: 10, left: 5, right: 5),
+                      child: new Card(
+                        child: Container(
+                          child: new Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.place,
+                                        color: Colors.grey,
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.only(top: 5),
+                                        child: Text(
+                                          myData[index]['vi_tri'] + "  -  ",
+                                          style: TextStyle(color: Colors.grey),
                                         ),
-                                        Container(
+                                      ),
+                                      Icon(
+                                        Icons.access_time,
+                                        color: Colors.grey,
+                                      ),
+                                      Container(
                                           padding: EdgeInsets.only(top: 5),
                                           child: Text(
-                                            myData[index]['vi_tri'] + "  -  ",
+                                            " " +
+                                                myData[index]
+                                                    ['thoi_gian_xay_ra'],
                                             style:
                                                 TextStyle(color: Colors.grey),
-                                          ),
+                                          )),
+                                      Container(
+                                        padding:
+                                            EdgeInsets.only(top: 5, left: 32),
+                                        child: Text(
+                                          myData[index]['tinh_trang_pa'],
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              color: Colors.blueAccent,
+                                              fontWeight: FontWeight.bold),
                                         ),
-                                        Icon(
-                                          Icons.access_time,
-                                          color: Colors.grey,
-                                        ),
-                                        Container(
-                                            padding: EdgeInsets.only(top: 5),
-                                            child: Text(
-                                              " " +
-                                                  myData[index]
-                                                      ['thoi_gian_xay_ra'],
-                                              style:
-                                                  TextStyle(color: Colors.grey),
-                                            )),
-                                        Container(
-                                          padding:
-                                              EdgeInsets.only(top: 5, left: 32),
-                                          child: Text(
-                                            myData[index]['tinh_trang_pa'],
-                                            style: TextStyle(
-                                                fontSize: 15,
-                                                color: Colors.blueAccent,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
+                                      ),
+                                    ],
                                   ),
-                                  new Container(
-                                      padding: EdgeInsets.only(left: 5, top: 5),
-                                      child: Text(
-                                        myData[index]['noi_dung'],
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                        ),
-                                      )),
-                                  Container(
-                                    padding: EdgeInsets.only(top: 5),
-                                    child: Row(
-                                      children: <Widget>[
-                                        Column(
-                                          mainAxisSize: MainAxisSize.min,
+                                ),
+                                new Container(
+                                    padding: EdgeInsets.only(left: 5, top: 5),
+                                    child: Text(
+                                      myData[index]['noi_dung'],
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    )),
+                                Container(
+                                  margin: EdgeInsets.only(left: 10),
+                                  width: SizeConfig.screenHeight,
+                                  height: SizeConfig.screenWidth,
+                                  child: Row(
+                                    children: <Widget>[
+                                      Container(
+                                        width:
+                                            SizeConfig.blockSizeHorizontal * 60,
+                                        height:
+                                            SizeConfig.blockSizeVertical * 58,
+                                        child: Column(
                                           children: <Widget>[
-                                            Container(
-                                              margin: EdgeInsets.only(right: 5),
-                                              padding:
-                                                  EdgeInsets.only(left: 10),
-                                              width: 262,
-                                              height: 225,
-                                              decoration: BoxDecoration(
-                                                  image: DecorationImage(
-                                                      image: NetworkImage(
-                                                          myData[index]
-                                                              ['hinh_anh']),
-                                                      fit: BoxFit.contain)),
-                                            )
+                                            Image.network(
+                                              myData[index]['hinh_anh'],
+                                            ),
                                           ],
                                         ),
-                                        Expanded(
-                                            child: Container(
-                                          child: Column(
-                                            mainAxisSize: MainAxisSize.min,
-                                            children: <Widget>[
-                                              Container(
-                                                width: 150,
-                                                height: 100,
-                                                decoration: BoxDecoration(
-                                                    image: DecorationImage(
-                                                        image: NetworkImage(
-                                                            myData[index]
-                                                                ['hinh_anh']),
-                                                        fit: BoxFit.contain)),
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.only(left: 5.0),
+                                        width:
+                                            SizeConfig.safeBlockHorizontal * 59,
+                                        height:
+                                            SizeConfig.safeBlockVertical * 67,
+                                        child: Column(
+                                          children: <Widget>[
+                                            Container(
+                                              margin:
+                                                  EdgeInsets.only(bottom: 3.0),
+                                              child: Image.network(
+                                                myData[index]['hinh_anh'],
+                                                fit: BoxFit.fill,
                                               ),
-                                              Container(
-                                                child: Image.network(
-                                                  myData[index]['hinh_anh'],
-                                                ),
+                                            ),
+                                            Container(
+                                              child: Image.network(
+                                                myData[index]['hinh_anh'],
+                                                fit: BoxFit.fill,
                                               ),
-                                            ],
-                                          ),
-                                        )),
-                                      ],
-                                    ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  new Container(
-                                    width: 400,
-                                    height: 200,
-                                    child: Image.network(
-                                        myData[index]['hinh_anh'],
-                                        fit: BoxFit.fill),
-                                  ),
-                                ]),
-                          ),
+                                ),
+                                new Container(
+                                  margin: EdgeInsets.only(
+                                      bottom: 5, left: 5, right: 5),
+                                  width: SizeConfig.screenHeight,
+                                  height: SizeConfig.screenWidth,
+                                  child: Image.network(
+                                      myData[index]['hinh_anh'],
+                                      fit: BoxFit.fill),
+                                ),
+                              ]),
                         ),
                       ),
-                      onTap: () {
-                        Navigator.pushNamed(context, '/detail',
-                            arguments: index);
-                      },
-                    );
+                    ),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/detail', arguments: index);
+                    },
+                  );
                 });
           },
           future: DefaultAssetBundle.of(context).loadString("assets/aaa.json"),
